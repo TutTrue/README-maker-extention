715114c5e770c4447d607d3f00440029ce5c04f0
Emad Anwer <emadanwer.official@gmail.com>
2023-07-09 18:56:42 +0200

    add Learning Objectives

--- background.js
+++ background.js
@@ -1,59 +1,88 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {
-      code: `
-        var projectName = document.title.split(" | ")[0];
-        var tasks = [];
-        var files = [];
-  
-        var mandatoryElements = document.querySelectorAll('span.label.label-info');
-        mandatoryElements.forEach(function(element) {
-          if (element.textContent.trim() === 'mandatory') {
-            var taskElement = element.closest('.panel-heading.panel-heading-actions').querySelector('h3.panel-title');
-            tasks.push(taskElement.textContent.trim());
-          }
-        });
-  
-        var advancedElements = document.querySelectorAll('span.label.label-info');
-        advancedElements.forEach(function(element) {
-          if (element.textContent.trim() === '#advanced') {
-            var taskElement = element.closest('.panel-heading.panel-heading-actions').querySelector('h3.panel-title');
-            tasks.push(taskElement.textContent.trim());
-          }
-        });
-  
-        var fileElements = document.querySelectorAll('.list-group-item ul li:nth-child(3)');
-        fileElements.forEach(function(element, index) {
-          var file = element.textContent.trim();
-          if (index < tasks.length) {
-            var task = tasks[index];
-            files.push({ task: task, file: file });
-          } else {
-            files.push({ task: '', file: file });
-          }
-        });
-  
-        [projectName, files];
-      `
-    }, function(results) {
-      var projectName = results[0][0];
-      var files = results[0][1];
-  
-      var copiedText = "# Project Name: " + projectName + "\n\n";
-      copiedText += "| Task | File |\n";
-      copiedText += "| ---- | ---- |\n";
-      files.forEach(function(pair) {
-        copiedText += "| " + pair.task + " | [" + pair.file.substring(6) + "](./"+ pair.file.substring(6) +") |\n";
-      });
-  
-      saveToFile(copiedText);
-    });
-  });
+  chrome.tabs.executeScript(tab.id, {
+    code: `
+      var projectName = document.title.split(" | ")[0];
+      var tasks = [];
+      var files = [];
+      var learningObjectives = [];
 
-  function saveToFile(text) {
-    var blob = new Blob([text], { type: 'text/plain' });
-    var fileName = 'README.md';
-    var downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = fileName;
-    downloadLink.click();
-  }
+      var mandatoryElements = document.querySelectorAll('span.label.label-info');
+      mandatoryElements.forEach(function(element) {
+        if (element.textContent.trim() === 'mandatory') {
+          var taskElement = element.closest('.panel-heading.panel-heading-actions').querySelector('h3.panel-title');
+          tasks.push(taskElement.textContent.trim());
+        }
+      });
+
+      var advancedElements = document.querySelectorAll('span.label.label-info');
+      advancedElements.forEach(function(element) {
+        if (element.textContent.trim() === '#advanced') {
+          var taskElement = element.closest('.panel-heading.panel-heading-actions').querySelector('h3.panel-title');
+          tasks.push(taskElement.textContent.trim());
+        }
+      });
+
+      var fileElements = document.querySelectorAll('.list-group-item ul li:nth-child(3)');
+      fileElements.forEach(function(element, index) {
+        var file = element.textContent.trim();
+        if (index < tasks.length) {
+          var task = tasks[index];
+          files.push({ task: task, file: file });
+        } else {
+          files.push({ task: '', file: file });
+        }
+      });
+
+      var learningObjectiveElements = document.querySelectorAll('h3');
+      var generalHeadingIndex = -1;
+      learningObjectiveElements.forEach(function(element, index) {
+        if (element.textContent.trim() === 'General' && generalHeadingIndex === -1) {
+          generalHeadingIndex = index;
+        }
+      });
+
+      if (generalHeadingIndex !== -1) {
+        var objectives = [];
+        var listElements = learningObjectiveElements[generalHeadingIndex].nextElementSibling.querySelectorAll('li');
+        listElements.forEach(function(liElement) {
+          objectives.push(liElement.textContent.trim());
+        });
+        learningObjectives.push({ category: 'General', objectives: objectives });
+      }
+
+      [projectName, files, learningObjectives];
+    `
+  }, function(results) {
+    var projectName = results[0][0];
+    var files = results[0][1];
+    var learningObjectives = results[0][2];
+
+    var copiedText = "# " + projectName + "\n\n";
+    copiedText += "<h2>Learning Objectives</h2>\n\n";
+    learningObjectives.forEach(function(obj) {
+      copiedText += "<h3>" + obj.category + "</h3>\n\n";
+      copiedText += "<ul>\n";
+      obj.objectives.forEach(function(objective) {
+        copiedText += "<li>" + objective + "</li>\n";
+      });
+      copiedText += "</ul>\n\n";
+    });
+
+    copiedText += "| Task | File |\n";
+    copiedText += "| ---- | ---- |\n";
+    files.forEach(function(pair) {
+      copiedText += "| " + pair.task + " | [" + pair.file.substring(6) + "](./"+ pair.file.substring(6) +") |\n";
+    });
+
+    saveToFile(copiedText);
+  });
+});
+
+function saveToFile(text) {
+  var blob = new Blob([text], { type: 'text/plain' });
+  var fileName = 'README.md';
+  var downloadLink = document.createElement('a');
+  downloadLink.href = URL.createObjectURL(blob);
+  downloadLink.download = fileName;
+  downloadLink.click();
+}
