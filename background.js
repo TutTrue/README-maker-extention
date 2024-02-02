c4490dfe176c7839ce72ca7f485310725832f258
husamahmud <husamahmud@gmail.com>
2024-02-03 02:34:51 +0300

    [fix] Update `background.js` for compatibility with v3 (#6)

--- background.js
+++ background.js
@@ -1,119 +1,84 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  if (tab.url.startsWith('https://intranet.alxswe.com/projects/')){
-  chrome.tabs.executeScript(tab.id, {
-    code: `
-      var projectName = document.title.split(" | ")[0];
-      var tasks = [];
-      var files = [];
-      var learningObjectives = [];
-      var resources = [];
-      
-      const taskElements = document.querySelectorAll('div[id^="task-num"]')
-      taskElements.forEach(taskDiv => {
-        const title = taskDiv.querySelector('h3.panel-title')?.outerText;
-        const task_type = taskDiv.querySelector('span.label.label-info')?.outerText;
+chrome.action.onClicked.addListener((tab) => {
+	chrome.scripting.executeScript({
+		target: {tabId: tab.id},
+		func: () => {
+			let general, link, url, title, taskTitle, taskMode, taskFileName,
+				readmeContent, fileName, string_files;
+			let learningObjs = [], resources = [], tasks = [];
+			const projName = document.title.split(': ')[1].split(' |')[0];
+			const headers = document.querySelectorAll('h2');
+			const tasksCards = document.querySelectorAll('div[id^=task-num]');
 
-        const files = taskDiv.querySelector('div.list-group-item ul li:nth-child(3) code')?.outerText;
-        files_array = files?.split(", ");
-        tasks.push({
-          title: title,
-          task_type: task_type,
-          files: files_array
-        });
-      });
+			for (const header of headers) {
+				if (header.textContent.includes('Learning Objectives')) {
+					general = header.nextElementSibling.nextElementSibling.nextElementSibling;
+					general.querySelectorAll('li').forEach(li =>
+						learningObjs.push(li.innerText)
+					);
+				}
 
-      var h2Elements = document.querySelectorAll('h2');
+				if (header.textContent.includes('Resources')) {
+					general = header.nextElementSibling.nextElementSibling;
+					general.querySelectorAll('li').forEach(li => {
+						link = li.querySelector('a');
+						url = link?.href || '';
+						title = link?.title || li.textContent;
+						resources.push({resource: title, link: url});
+					});
+				}
+			}
 
-      for (var i = 0; i < h2Elements.length; i++) {
-          if (h2Elements[i].textContent.includes("Learning Objectives")) {
-            var general = h2Elements[i].nextElementSibling.nextElementSibling.nextElementSibling;
-            general.querySelectorAll("li").forEach(li =>{
-              learningObjectives.push(li.innerHTML);
-            });
-            break;
-          }
-      }
+			tasksCards.forEach(taskCard => {
+				taskTitle = taskCard.querySelector('h3.panel-title').outerText;
+				taskMode = taskCard.querySelector('span.label.label-info').outerText;
+				taskFileName = taskCard.querySelector('div.list-group-item ul li:last-child code').outerText;
+				tasks.push({
+					taskTitle: taskTitle,
+					taskMode: taskMode,
+					taskFileName: taskFileName
+				});
+			});
 
-      var h2Elements = document.querySelectorAll('h2');
+			readmeContent = `# ${projName}\n\n`;
 
-      for (var i = 0; i < h2Elements.length; i++) {
-          if (h2Elements[i].textContent.includes("Resources")) {
-            var general = h2Elements[i].nextElementSibling.nextElementSibling;
-            general.querySelectorAll("li").forEach(li =>{
-                var link = li.querySelector("a");
-                var url = link && link.href ? link.href : "";
-                var title = link && link.title ? link.title : li.textContent;
+			readmeContent += '## Resources\n\n';
+			readmeContent += '#### Read or watch:\n\n';
+			resources.forEach((obj) => {
+				readmeContent += `* [${obj.resource}](${obj.link})\n`;
+			});
 
-                resources.push({ resource: title, link: url });
-            });
-            break;
-          }
-      }
+			readmeContent += '## Learning Objectives\n\n';
+			readmeContent += '### General\n\n';
+			learningObjs.forEach(function (obj) {
+				readmeContent += '* ' + obj + '\n';
+			});
 
-      [projectName, tasks, learningObjectives, resources];
-    `
-  }, function(results) {
-    var projectName = results[0][0];
-    var tasks = results[0][1];
-    var learningObjectives = results[0][2];
-    var resources = results[0][3];
+			readmeContent += '## Tasks\n\n';
+			readmeContent += '| Task | File | Mode |\n';
+			readmeContent += '| ---- | ---- | ---- |\n';
+			tasks.forEach(function (task) {
+				string_files = '';
+				fileName = task.taskFileName.split(', ');
+				fileName.forEach(file => {
+					string_files += `[${file}](./${file}), `;
+				});
+				string_files = string_files.slice(0, -2);
+				readmeContent += `| ${task.taskTitle} | ${string_files} | ${task.taskMode} |\n`;
+			});
 
-    var copiedText = `# ${projectName}\n\n`;
+			const blob = new Blob([readmeContent], {type: 'text/plain'});
+			const readmeFile = 'README.md';
+			const downloadLink = document.createElement('a');
+			downloadLink.href = URL.createObjectURL(blob);
+			downloadLink.download = readmeFile;
+			downloadLink.click();
 
-    if (resources.length !== 0) {
-      copiedText += "## Resources\n\n";
-      copiedText += "#### Read or watch:\n\n";
-      resources.forEach(function(obj){
-        copiedText += `* [${obj.resource}](${obj.link})\n`;
-      });
-    }
-
-    if (learningObjectives.length !== 0) {
-      copiedText += "## Learning Objectives\n\n";
-      copiedText += "### General\n\n";
-      learningObjectives.forEach(function(obj) {
-        copiedText += "* " + obj + "\n";
-      });
-    }
-
-	  copiedText += "## Tasks\n\n";
-    copiedText += "| Task | File |\n";
-    copiedText += "| ---- | ---- |\n";
-
-    tasks.forEach(function(task) {
-      let string_files = "";
-      if (task.files != null) {
-        task.files.forEach((file)=>{
-          string_files+= `[${file}](./${file}), `;
-        })
-        string_files = string_files.slice(0,-2)
-      }
-      else {
-        string_files = "[SOON](./)"
-      }
-      copiedText += `| ${task.title} | ${string_files} |\n`;
-    });
-
-    copyToClipboard(copiedText);
-    saveToFile(copiedText);
-  });
-  }
+			const textArea = document.createElement('textarea');
+			textArea.value = readmeContent;
+			document.body.appendChild(textArea);
+			textArea.select();
+			document.execCommand('copy');
+			document.body.removeChild(textArea);
+		}
+	});
 });
-
-function saveToFile(text) {
-  var blob = new Blob([text], { type: 'text/plain' });
-  var fileName = 'README.md';
-  var downloadLink = document.createElement('a');
-  downloadLink.href = URL.createObjectURL(blob);
-  downloadLink.download = fileName;
-  downloadLink.click();
-}
-
-function copyToClipboard(text) {
-  var textArea = document.createElement("textarea");
-  textArea.value = text;
-  document.body.appendChild(textArea);
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
-}
